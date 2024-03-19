4c5ad549901dc749a8e455a9098bbdbfa367279d
noname <48761044+noname0310@users.noreply.github.com>
2024-03-20 07:45:47 +0900

    Update lib.h

--- src/wasm_src/cpp_src/lib.h
+++ src/wasm_src/cpp_src/lib.h
@@ -1,5 +1,6 @@
 // for use simd instructions
 #define _WIN32
+#define _MSC_VER 1401
 #define __i386__
 #define __SSE__
 #define __SSE2__
@@ -7,8 +8,12 @@
 #define __SSSE3__
 #define __SSE4_1__
 
-#include "LinearMath/btScalar.h"
+#define BT_USE_SSE
+#define BT_USE_SSE_IN_API
+#define BT_NO_SIMD_OPERATOR_OVERLOADS
+
 #include <smmintrin.h>
+#include "LinearMath/btScalar.h"
 
 extern "C" {
     extern void __wasm_call_ctors();
