87f25f817476c9bb1734762080a831b6163994b2
noname <48761044+noname0310@users.noreply.github.com>
2024-06-06 19:51:34 +0900

    binding wip

--- src/wasm_src/cpp_src/lib.cpp
+++ src/wasm_src/cpp_src/lib.cpp
@@ -37,203 +37,105 @@ void operator delete[](void* ptr, size_t size) noexcept {
 
 //
 
-#include "LinearMath/btMinMax.h"
-#include "LinearMath/btScalar.h"
-#include "LinearMath/btVector3.h"
-#include "LinearMath/btQuadWord.h"
-#include "LinearMath/btQuaternion.h"
-#include "LinearMath/btMatrix3x3.h"
-#include "LinearMath/btTransform.h"
-#include "LinearMath/btAlignedAllocator.h"
-#include "LinearMath/btAlignedObjectArray.h"
-#include "BulletCollision/BroadphaseCollision/btBroadphaseProxy.h"
-#include "LinearMath/btMotionState.h"
-#include "LinearMath/btHashMap.h"
-#include "LinearMath/btSerializer.h"
-#include "BulletCollision/CollisionDispatch/btCollisionObject.h"
-#include "BulletCollision/CollisionShapes/btCollisionShape.h"
-#include "BulletCollision/CollisionShapes/btCollisionMargin.h"
-#include "LinearMath/btConvexHullComputer.h"
-#include "LinearMath/btGeometryUtil.h"
-#include "LinearMath/btGrahamScan2dConvexHull.h"
-#include "BulletCollision/CollisionShapes/btConvexPolyhedron.h"
-#include "LinearMath/btAabbUtil2.h"
-#include "BulletCollision/CollisionShapes/btConvexInternalShape.h"
-#include "BulletCollision/CollisionShapes/btPolyhedralConvexShape.h"
-#include "BulletCollision/CollisionShapes/btBoxShape.h"
-#include "BulletCollision/CollisionShapes/btTriangleShape.h"
-#include "BulletCollision/CollisionShapes/btSphereShape.h"
-#include "BulletCollision/CollisionShapes/btCylinderShape.h"
-#include "BulletCollision/CollisionShapes/btConeShape.h"
-#include "BulletCollision/CollisionShapes/btCapsuleShape.h"
-#include "BulletCollision/CollisionShapes/btConvexHullShape.h"
-#include "BulletCollision/CollisionShapes/btConvexPointCloudShape.h"
-#include "BulletCollision/CollisionShapes/btConvexShape.h"
-#include "LinearMath/btTransformUtil.h"
-#include "BulletDynamics/ConstraintSolver/btSolverBody.h"
-#include "BulletDynamics/ConstraintSolver/btJacobianEntry.h"
-#include "BulletDynamics/ConstraintSolver/btSolverConstraint.h"
-#include "BulletDynamics/ConstraintSolver/btTypedConstraint.h"
-#include "BulletDynamics/Dynamics/btRigidBody.h"
-#include "LinearMath/btThreads.h"
-#include "BulletCollision/BroadphaseCollision/btBroadphaseInterface.h"
-#include "BulletCollision/BroadphaseCollision/btDbvt.h"
-#include "BulletCollision/BroadphaseCollision/btOverlappingPairCallback.h"
-#include "BulletCollision/BroadphaseCollision/btCollisionAlgorithm.h"
-#include "BulletCollision/BroadphaseCollision/btDispatcher.h"
-#include "BulletCollision/BroadphaseCollision/btOverlappingPairCache.h"
-#include "BulletCollision/BroadphaseCollision/btDbvtBroadphase.h"
-#include "BulletCollision/CollisionDispatch/btActivatingCollisionAlgorithm.h"
-#include "BulletCollision/NarrowPhaseCollision/btGjkPairDetector.h"
-#include "BulletCollision/CollisionDispatch/btConvexConvexAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btEmptyCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btConvexConcaveCollisionAlgorithm.h"
-#include "BulletCollision/CollisionShapes/btMiniSDF.h"
-#include "BulletCollision/CollisionDispatch/btCompoundCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btCompoundCompoundCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btConvexPlaneCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btBoxBoxCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btSphereSphereCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btSphereTriangleCollisionAlgorithm.h"
-#include "BulletCollision/CollisionDispatch/btDefaultCollisionConfiguration.h"
-#include "BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h"
-#include "BulletCollision/CollisionDispatch/btCollisionWorld.h"
-#include "BulletCollision/CollisionDispatch/btCollisionDispatcher.h"
-#include "BulletCollision/CollisionDispatch/btSimulationIslandManager.h"
-#include "BulletDynamics/Dynamics/btDiscreteDynamicsWorld.h"
+#include "btBulletDynamicsMinimal.h"
 
-// test extern functions
+// extern functions
 
-extern "C" int bt_get_version() {
-    return btGetVersion();
-}
+class bwPhysicsWorld final {
+private:
+    btDbvtBroadphase* m_broadphase;
+    btDefaultCollisionConfiguration* m_collisionConfig;
+    btCollisionDispatcher* m_dispatcher;
+    btSequentialImpulseConstraintSolver* m_solver;
+    btDiscreteDynamicsWorld* m_world;
 
-extern "C" float bt_sin(float x) {
-    return btSin(x);
-}
+public:
+    bwPhysicsWorld() {
+        m_broadphase = new btDbvtBroadphase();
+        m_collisionConfig = new btDefaultCollisionConfiguration();
+        m_dispatcher = new btCollisionDispatcher(m_collisionConfig);
+        m_solver = new btSequentialImpulseConstraintSolver();
+        m_world = new btDiscreteDynamicsWorld(m_dispatcher, m_broadphase, m_solver, m_collisionConfig);
+    }
 
-extern "C" int* bt_alloc_int() {
-    int* boxed = new int(3);
-    return boxed;
-}
+    bwPhysicsWorld(bwPhysicsWorld const&) = delete;
+    bwPhysicsWorld& operator=(bwPhysicsWorld const&) = delete;
 
-extern "C" int* bt_nonallocnew_test() {
-    void* ptr = malloc(sizeof(int));
-    int* boxed = new(ptr) int(3);
-    return boxed;
-}
-
-extern "C" void bt_free_int(int* ptr) {
-    delete ptr;
-}
-
-extern "C" void bt_transform_test() {
-    btTransform t1;
-    btTransform t2;
-    btTransform t3 = t1 * t2;
-}
-
-extern "C" int bt_vector3_test() {
-    btVector3 v1(1, 2, 3);
-    btVector3 v2(4, 5, 6);
-    btVector3 v3 = v1 + v2;
-    return v3.x() + v3.y() + v3.z();
-}
-
-extern "C" void* bt_create_dbvtbroadphase() {
-    btDbvtBroadphase* broadphase = new btDbvtBroadphase();
-    return broadphase;
-}
-
-extern "C" void bt_delete_dbvtbroadphase(void* broadphase) {
-    delete static_cast<btDbvtBroadphase*>(broadphase);
-}
-
-extern "C" void* bt_create_rigidbody() {
-    btRigidBody::btRigidBodyConstructionInfo info(0, 0, 0);
-    btRigidBody* body = new btRigidBody(info);
-    return body;
-}
-
-extern "C" void bt_delete_rigidbody(void* body) {
-    delete static_cast<btRigidBody*>(body);
-}
-
-extern "C" void bt_link_test() {
-    btSphereShape shape1(1);
-    btSphereShape shape2(1);
-    btGjkPairDetector detector(&shape1, &shape2, 0, 0);
-
-    btRigidBody body1(0, 0, 0);
-
-    class Dispatcher : public btDispatcher {
-        btCollisionAlgorithm* findAlgorithm(const btCollisionObjectWrapper* body0Wrap, const btCollisionObjectWrapper* body1Wrap, btPersistentManifold* sharedManifold, ebtDispatcherQueryType queryType) {
-            return nullptr;
-        }
-        btPersistentManifold* getNewManifold(const btCollisionObject* b0, const btCollisionObject* b1) {
-            return nullptr;
-        }
-        void releaseManifold(btPersistentManifold* manifold) {}
-        void clearManifold(btPersistentManifold* manifold) {}
-        bool needsCollision(const btCollisionObject* body0, const btCollisionObject* body1) {
-            return false;
-        }
-        bool needsResponse(const btCollisionObject* body0, const btCollisionObject* body1) {
-            return false;
-        }
-        void dispatchAllCollisionPairs(btOverlappingPairCache* pairCache, const btDispatcherInfo& dispatchInfo, btDispatcher* dispatcher) {}
-        int getNumManifolds() const {
-            return 0;
-        }
-        btPersistentManifold* getManifoldByIndexInternal(int index) {
-            return nullptr;
-        }
-
-        btPersistentManifold** getInternalManifoldPointer() {
-            return nullptr;
-        }
-
-        btPoolAllocator* getInternalManifoldPool() {
-            return nullptr;
-        }
-
-        const btPoolAllocator* getInternalManifoldPool() const {
-            return nullptr;
-        }
-
-        void* allocateCollisionAlgorithm(int size) {
-            return nullptr;
-        }
-
-        void freeCollisionAlgorithm(void* algo) {}
-    };
-
-    btPersistentManifold manifold;
+    ~bwPhysicsWorld() {
+        delete m_world;
+        delete m_solver;
+        delete m_dispatcher;
+        delete m_collisionConfig;
+        delete m_broadphase;
+    }
     
-    Dispatcher dispatcher1;
-    btCollisionAlgorithmConstructionInfo info1(&dispatcher1, 0);
-    btCollisionObjectWrapper wrapper1(nullptr, &shape1, &body1, btTransform::getIdentity(), 0, 0);
-    btCollisionObjectWrapper wrapper2(nullptr, &shape2, &body1, btTransform::getIdentity(), 0, 0);
-    btConvexConvexAlgorithm algorithm(&manifold, info1, nullptr, nullptr, nullptr, 0, 0);
-    
-    Dispatcher dispatcher2;
-    btCollisionAlgorithmConstructionInfo info2(&dispatcher2, 0);
-    btEmptyAlgorithm empty_algorithm(info2);
+    void setGravity(btScalar x, btScalar y, btScalar z) {
+        m_world->setGravity(btVector3(x, y, z));
+    }
 
-    Dispatcher dispatcher3;
-    btCollisionAlgorithmConstructionInfo info3(&dispatcher3, 0);
-    btConvexConcaveCollisionAlgorithm concave_algorithm(info3, &wrapper1, &wrapper2, false);
-    btMiniSDF sdf;
-    double s;
-    btVector3 v;
-    sdf.interpolate(0, s, btVector3(0, 0, 0), &v);
+    void stepSimulation(btScalar timeStep, int maxSubSteps, btScalar fixedTimeStep) {
+        m_world->stepSimulation(timeStep, maxSubSteps, fixedTimeStep);
+    }
 
-    btDbvtBroadphase broadphase;
-    btDefaultCollisionConfiguration collision_config;
-    btCollisionDispatcher dispatcher(&collision_config);
-    btSequentialImpulseConstraintSolver solver;
+    void addRigidBody(btRigidBody* body) {
+        m_world->addRigidBody(body);
+    }
 
-    btDiscreteDynamicsWorld world(&dispatcher, &broadphase, &solver, &collision_config);
+    void removeRigidBody(btRigidBody* body) {
+        m_world->removeRigidBody(body);
+    }
+
+    void addConstraint(btTypedConstraint* constraint) {
+        m_world->addConstraint(constraint);
+    }
+
+    void removeConstraint(btTypedConstraint* constraint) {
+        m_world->removeConstraint(constraint);
+    }
+};
+
+extern "C" void* bt_create_world() {
+    bwPhysicsWorld* world = new bwPhysicsWorld();
+    return world;
+}
+
+extern "C" void bt_destroy_world(void* world) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    delete w;
+}
+
+extern "C" void bt_world_set_gravity(void* world, float x, float y, float z) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    w->setGravity(x, y, z);
+}
+
+extern "C" void bt_world_step_simulation(void* world, float timeStep, int maxSubSteps, float fixedTimeStep) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    w->stepSimulation(timeStep, maxSubSteps, fixedTimeStep);
+}
+
+extern "C" void bt_world_add_rigidbody(void* world, void* body) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    btRigidBody* b = static_cast<btRigidBody*>(body);
+    w->addRigidBody(b);
+}
+
+extern "C" void bt_world_remove_rigidbody(void* world, void* body) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    btRigidBody* b = static_cast<btRigidBody*>(body);
+    w->removeRigidBody(b);
+}
+
+extern "C" void bt_world_add_constraint(void* world, void* constraint) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    btTypedConstraint* c = static_cast<btTypedConstraint*>(constraint);
+    w->addConstraint(c);
+}
+
+extern "C" void bt_world_remove_constraint(void* world, void* constraint) {
+    bwPhysicsWorld* w = static_cast<bwPhysicsWorld*>(world);
+    btTypedConstraint* c = static_cast<btTypedConstraint*>(constraint);
+    w->removeConstraint(c);
 }
 
 //
--- src/wasm_src/src/lib.rs
+++ src/wasm_src/src/lib.rs
@@ -1,11 +1,8 @@
-mod math;
-mod allocator;
-mod error_handler;
-mod atomic;
+mod bind;
+mod cpp_runtime;
 
-// use atomic::{bw_mutex_init, bw_mutex_lock, bw_mutex_unlock, bw_get_thread_id, UnsafeManualMutex};
+use bind::physics_world::PhysicsWorld;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 // use rayon::prelude::*;
 
 pub use wasm_bindgen_rayon::init_thread_pool;
@@ -13,30 +10,6 @@ pub use wasm_bindgen_rayon::init_thread_pool;
 #[link(name = "bullet")]
 extern "C" {
     fn __wasm_call_ctors();
-
-    fn bt_get_version() -> i32;
-
-    fn bt_sin(x: f32) -> f32;
-
-    fn bt_alloc_int() -> *mut i32;
-
-    fn bt_free_int(ptr: *mut i32);
-
-    fn bt_nonallocnew_test() -> *mut i32;
-
-    fn bt_transform_test();
-
-    fn bt_vector3_test() -> i32;
-
-    fn bt_create_dbvtbroadphase() -> *mut core::ffi::c_void;
-
-    fn bt_delete_dbvtbroadphase(ptr: *mut core::ffi::c_void);
-
-    fn bt_create_rigidbody() -> *mut core::ffi::c_void;
-
-    fn bt_delete_rigidbody(ptr: *mut core::ffi::c_void);
-
-    fn bt_link_test();
 }
 
 #[wasm_bindgen(js_name = init)]
@@ -50,78 +23,9 @@ pub fn init() {
 
 #[wasm_bindgen(js_name = test)]
 pub fn test() {
-    unsafe {
-        let version = bt_get_version();
-        console::log_1(&format!("Bullet version: {}", version).into());
-
-        let sin = bt_sin(1.0);
-        console::log_1(&format!("sin(1.0): {}", sin).into());
-
-        let boxed_int = bt_alloc_int();
-        console::log_1(&format!("boxed_int: {:?}", *boxed_int).into());
-        bt_free_int(boxed_int);
-
-        let boxed_int = bt_nonallocnew_test();
-        console::log_1(&format!("boxed_int: {:?}", *boxed_int).into());
-        bt_free_int(boxed_int);
-
-        bt_transform_test();
-
-        let vector3_test = bt_vector3_test();
-        console::log_1(&format!("vector3_test: {}", vector3_test).into());
-
-        let dbvtbroadphase = bt_create_dbvtbroadphase();
-        bt_delete_dbvtbroadphase(dbvtbroadphase);
-
-        let rigidbody = bt_create_rigidbody();
-        bt_delete_rigidbody(rigidbody);
-
-        for _ in 0..10 {
-            rayon::spawn(move || {
-                bt_link_test();
-            });
-        }
-    }
-
-    // wasmsync test
-
-    // let mut mutex = wasm_sync::Mutex::new(0);
-    // for _ in 0..10 {
-    //     let mutex_ptr = &mut mutex as *mut wasm_sync::Mutex<i32> as usize;
-    //     rayon::spawn(move || {
-    //         let mutex = unsafe { &mut *(mutex_ptr as *mut wasm_sync::Mutex<i32>) };
-    //         let mut data = mutex.lock().unwrap();
-    //         *data += 1;
-    //         console::log_1(&format!("counter: {}", *data).into());
-    //     });
-    // }
-
-    // mutex test
-
-    // let unsafe_manual_mutex = bw_mutex_init();
-    // let counter = Box::new(0);
-    // let counter = Box::leak(counter) as *mut i32;
-
-    // for _ in 0..1000 {
-    //     let mutex = unsafe_manual_mutex as usize;
-    //     let counter = counter as usize;
-
-    //     rayon::spawn(move || {
-    //         let mutex = mutex as *mut UnsafeManualMutex;
-    //         let counter = counter as *mut i32;
-
-    //         let lock_result = bw_mutex_lock(mutex);
-    //         if lock_result != 0 {
-    //             console::log_1(&"lock failed".into());
-    //         }
-    //         unsafe {
-    //             *counter += 1;
-    //             console::log_1(&format!("counter: {} thread: {}", *counter, bw_get_thread_id()).into());
-    //         }
-    //         let unlock_result = bw_mutex_unlock(mutex);
-    //         if unlock_result != 0 {
-    //             console::log_1(&"unlock failed".into());
-    //         }
-    //     });
-    // } 
+    for _ in 0..10000000 {
+        rayon::spawn(move || {
+            let world = PhysicsWorld::new();
+        });
+    } 
 }
